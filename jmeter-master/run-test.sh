c65c367d6833d5f687ba6f8e6f277d3257614781
Captain.B <bin@fit2cloud.com>
2020-12-21 15:02:36 +0800

    修改镜像的脚本

--- jmeter-master/run-test.sh
+++ jmeter-master/run-test.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate "${METERSPHERE_URL}/jmeter/download?testId=${TEST_ID}&resourceId=${RESOURCE_ID}&ratio=${RATIO}&startTime=${START_TIME}&reportId=${REPORT_ID}&resourceIndex=${RESOURCE_INDEX}" -O ${TEST_ID}.zip
+wget --no-check-certificate "${METERSPHERE_URL}/jmeter/download?testId=${TEST_ID}&resourceId=${RESOURCE_ID}&ratio=${RATIO}&startTime=${START_TIME}&reportId=${REPORT_ID}&resourceIndex=${RESOURCE_INDEX}&threadNum=${THREAD_NUM}" -O ${TEST_ID}.zip
 unzip -d ${TESTS_DIR}/${TEST_ID}.zip
 for file in ${TESTS_DIR}/*.jmx; do
   jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED} -l ${TESTS_DIR}/${REPORT_ID}.jtl
