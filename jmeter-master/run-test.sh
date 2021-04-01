d36240bc1f3a77d22a300e42561a2033e15d361d
Captain.B <bin@fit2cloud.com>
2021-04-01 16:12:54 +0800

    refactor: 合并DockerFile, 简化构建流程

--- jmeter-master/run-test.sh
+++ jmeter-master/run-test.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate "${METERSPHERE_URL}/jmeter/download?testId=${TEST_ID}&resourceId=${RESOURCE_ID}&ratio=${RATIO}&startTime=${START_TIME}&reportId=${REPORT_ID}&resourceIndex=${RESOURCE_INDEX}&threadNum=${THREAD_NUM}" -O ${TEST_ID}.zip
+wget --no-check-certificate "${METERSPHERE_URL}/jmeter/download?testId=${TEST_ID}&resourceId=${RESOURCE_ID}&ratio=${RATIO}&reportId=${REPORT_ID}&resourceIndex=${RESOURCE_INDEX}" -O ${TEST_ID}.zip
 unzip -o ${TEST_ID}.zip -d ${TESTS_DIR} 
 for file in ${TESTS_DIR}/*.jmx; do
   jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED}
