af5e96e2a0157f09e4c301251ad03f3f3e667e0f
liuruibin <bin@fit2cloud.com>
2021-04-05 17:35:40 +0800

    refactor: 提取变量

--- jmeter-master/build-jmeter.sh
+++ jmeter-master/build-jmeter.sh
@@ -13,18 +13,18 @@ wget "http://search.maven.org/remotecontent?filepath=com/oracle/database/jdbc/oj
 wget "http://search.maven.org/remotecontent?filepath=org/postgresql/postgresql/42.2.14/postgresql-42.2.14.jar" -O postgresql.jar && \
 wget "http://search.maven.org/remotecontent?filepath=com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre8/mssql-jdbc-7.4.1.jre8.jar" -O mssql-jdbc.jar && \
 wget "http://search.maven.org/remotecontent?filepath=io/metersphere/jmeter-plugins-threadgroup-autostop/0.1/jmeter-plugins-threadgroup-autostop-0.1.jar" -O jmeter-plugins-threadgroup-autostop.jar && \
-mkdir -p /opt/jmeter && \
-tar -zxf apache-jmeter-${JMETER_VERSION}.tgz -C /opt/jmeter/  --strip-components=1 && \
-unzip -o jpgc-casutg-2.9.zip -d /tmp/ && mv /tmp/lib/ext/jmeter-plugins-casutg-2.9.jar /opt/jmeter/lib/ext && \
-unzip -o jpgc-tst-2.5.zip -d /tmp/ && mv /tmp/lib/ext/jmeter-plugins-tst-2.5.jar /opt/jmeter/lib/ext && \
-mv jmeter.backendlistener.kafka-${KAFKA_BACKEND_LISTENER_VERSION}.jar /opt/jmeter/lib/ext && \
-mv jmeter-plugins-dubbo-${DUBBO_JMETER_PLUGIN_VERSION}-jar-with-dependencies.jar /opt/jmeter/lib/ext && \
-mv mysql-connector-java.jar /opt/jmeter/lib/ext && \
-mv ojdbc8.jar /opt/jmeter/lib/ext && \
-mv postgresql.jar /opt/jmeter/lib/ext && \
-mv mssql-jdbc.jar /opt/jmeter/lib/ext && \
-mv jmeter-plugins-threadgroup-autostop.jar /opt/jmeter/lib/ext && \
-mv jython-standalone-2.7.2.jar /opt/jmeter/lib/ext && \
+mkdir -p ${JMETER_HOME} && \
+tar -zxf apache-jmeter-${JMETER_VERSION}.tgz -C ${JMETER_HOME}/  --strip-components=1 && \
+unzip -o jpgc-casutg-2.9.zip -d /tmp/ && mv /tmp/lib/ext/jmeter-plugins-casutg-2.9.jar ${JMETER_HOME}/lib/ext && \
+unzip -o jpgc-tst-2.5.zip -d /tmp/ && mv /tmp/lib/ext/jmeter-plugins-tst-2.5.jar ${JMETER_HOME}/lib/ext && \
+mv jmeter.backendlistener.kafka-${KAFKA_BACKEND_LISTENER_VERSION}.jar ${JMETER_HOME}/lib/ext && \
+mv jmeter-plugins-dubbo-${DUBBO_JMETER_PLUGIN_VERSION}-jar-with-dependencies.jar ${JMETER_HOME}/lib/ext && \
+mv mysql-connector-java.jar ${JMETER_HOME}/lib/ext && \
+mv ojdbc8.jar ${JMETER_HOME}/lib/ext && \
+mv postgresql.jar ${JMETER_HOME}/lib/ext && \
+mv mssql-jdbc.jar ${JMETER_HOME}/lib/ext && \
+mv jmeter-plugins-threadgroup-autostop.jar ${JMETER_HOME}/lib/ext && \
+mv jython-standalone-2.7.2.jar ${JMETER_HOME}/lib/ext && \
 rm -rf apache-jmeter-${JMETER_VERSION}.tgz && \
 rm -rf jpgc-casutg-2.9.zip && \
 rm -rf jpgc-tst-2.5.zip && \
